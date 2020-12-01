eabd87933765f417783d1a8bb8e63581bd13ffe7
Martin Mendoza <mendozanmartin@gmail.com>
2020-12-01 17:54:32 -0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.6.8
+
+RUN mkdir -p /usr/src/flask_app/
+COPY requirements.txt /usr/src/flask_app/
+
+WORKDIR /usr/src/flask_app/
+RUN pip install -r requirements.txt
+
+COPY . /usr/src/flask_app
+
+CMD ["flask", "run", "--host", "0.0.0.0"]
+
+EXPOSE 5000
---
+++ app.py
@@ -0,0 +1,39 @@
+# allow both GET and POST requests
+from flask import Flask, request, send_from_directory
+import numpy as np
+
+from keras.models import load_model
+from skimage.transform import resize
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input
+
+
+app = Flask(__name__)
+new_model = load_model('ASL.h5')
+
+def predict(frame):
+    print("Predicting")
+    # img = resize(frame, (64, 64, 3))
+    img_array = image.img_to_array(frame)
+    img_batch = np.expand_dims(img_array, axis=0)
+    img_preprocessed = preprocess_input(img_batch)
+    prediction = new_model.predict(img_preprocessed)
+    print(prediction)
+
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    if request.method == 'POST':  # this block is only entered when the form is submitted
+        json = request.get_json()
+        print(json['string'])
+    return "A"
+
+@app.route('/', methods=['GET'])
+def landing_page():
+    img_path = "test images/Y Test.jpg"
+    img = image.load_img(img_path, target_size=(64,64)) # resize image
+    print(predict(img))
+    return "<h1>WELCOME TO THE CIRCUS</h1>"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
